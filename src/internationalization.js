7201d8bb743f0e03d79b56675ece95659300e4b0
DewBaunchee <vmegabit@gmail.com>
2021-05-11 13:16:47 +0300

    Locales

--- src/internationalization.js
+++ src/internationalization.js
@@ -8,6 +8,7 @@ i18next
     .use(Cache)
     .use(initReactI18next)
     .init({
+        loadPath: '/locales/{{lng}}/{{ns}}.json',
         fallbackLng: 'en',
         debug: true,
         returnObjects: true,
